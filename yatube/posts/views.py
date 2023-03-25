2e68c3f105d5959d65e96081332adf95372a6320
Артём Серов (д) <artyomserov@yandex.ru>
2023-03-25 23:10:15 +0500

    Добавлен вызов полей title в html из views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 
 
 def index(request):
+    # Передаем адрес шаблона в переменную
     template = 'posts/index.html'
-    return render(request, template)
+    # Передаем строку для вывода из словаря
+    title_index = 'Это главная страница проекта Yatube'
+    text_index = 'Последние обновления на сайте.'
+    context = {
+        'title': title_index,
+        'text': text_index
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title_group = 'Здесь будет информация о группах проекта Yatube'
+    text_group = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'title': title_group,
+        'text': text_group
+    }
+    return render(request, template, context)
