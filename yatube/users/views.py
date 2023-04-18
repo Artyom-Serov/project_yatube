4e89dba31726fb7e178da5e1932948630dd68bd8
Артём Серов (д) <artyomserov@yandex.ru>
2023-04-19 01:36:47 +0500

    Добавлено восстановление пароля

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,10 +1,13 @@
+
 from django.views.generic import CreateView, TemplateView
-from django.contrib.auth.views import PasswordChangeView, PasswordResetView
+from django.contrib.auth.views import (
+    PasswordChangeView,
+    PasswordResetView,
+    PasswordResetConfirmView)
 from django.urls import reverse_lazy
 # Функция reverse_lazy позволяет получить URL по параметрам функции path().
 from .forms import CreationForm
 # Импортируем класс формы, чтобы сослаться на неё во view-классе
-from django.core.mail import send_mail
 
 
 class SignUp(CreateView):
@@ -18,19 +21,20 @@ class PasswordChange(PasswordChangeView):
     success_url = reverse_lazy('users:password_change_done')
     template_name = 'users/password_change_form.html'
 
-    send_mail(
-        'Cмена пароля',
-        'Текст письма.',
-        'from@example.com',  # Это поле "От кого"
-        ['to@example.com'],  # Это поле "Кому" (можно указать список адресов)
-        fail_silently=False,  # Сообщать об ошибках («молчать ли об ошибках?»)
-    )
-
 
 class PasswordChangeDoneView(TemplateView):
     template_name = 'users/password_change_done.html'
 
 
-class PasswordResetView(PasswordResetView):
+class PasswordReset(PasswordResetView):
     success_url = reverse_lazy('users:password_reset_done')
     template_name = 'users/password_reset_form.html'
+
+
+class PasswordResetDoneView(TemplateView):
+    template_name = 'users/password_reset_done.html'
+
+
+class PasswordResetConfirm(PasswordResetConfirmView):
+    success_url = reverse_lazy('users:password_reset_complete')
+    template_name = 'users/password_reset_confirm.html'
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -147,3 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 # указываем директорию, в которую будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+PASSWORD_RESET_EMAIL_TEMPLATE = 'users/password_reset_email.html'
