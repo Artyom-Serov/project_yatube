05f3c11757526cedbebe2fd19da5c63ed5c63ffa
Artyom-Serov <artyomserov@yandex.ru>
2023-05-04 01:54:23 +0500

    Добавление изображения в новый пост

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -79,7 +79,7 @@ def post_detail(request, post_id):
 @login_required
 def post_create(request):
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, files=request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
