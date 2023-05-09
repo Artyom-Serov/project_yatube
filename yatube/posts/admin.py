11cd46877bbe25dc88173e23b19b1d645825743b
Артём Серов (д) <artyomserov@yandex.ru>
2023-05-09 20:17:27 +0500

    Редактирование admin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Из модуля models импортируем моделb Post, Group
-from .models import Post, Group
+from .models import Post, Group, Comment, Follow
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Модель админки."""
+    """Модель администрирования постов."""
     list_display = (
         'pk',
         'text',
@@ -24,7 +25,46 @@ class PostAdmin(admin.ModelAdmin):
     # Свойство отображения пустых колонок
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
-admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    """Модель администрирования групп."""
+    list_display = (
+        'title',
+        'slug',
+        'description',
+    )
+    # отображаемые поля в админке
+    search_fields = ('slug',)
+    # Интерфейс для поиска по слагу
+    list_filter = ('title',)
+    # Возможность фильтрации по названию группы
+    empty_value_display = '-пусто-'
+    # Свойство отображения пустых колонок
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Модель администрирования комментариев."""
+    list_display = (
+        'post',
+        'author',
+        'text',
+        'created',
+    )
+    # отображаемые поля в админке
+    list_filter = ('author', 'text', 'created')
+    # Возможность фильтрации по названию автору,
+    # полю коментариев и дате создания
+    search_fields = ('post', 'author', 'text')
+    # Интерфейс для поиска по заданым полям
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    """Модель администрирования подписок."""
+    list_display = ('user', 'author')
+    # отображаемые поля в админке
+    list_filter = ('user', 'author')
+    # Возможность фильтрации по названию автору и подписчику
+    search_fields = ('user', 'author')
+    # Интерфейс для поиска по заданым полям
